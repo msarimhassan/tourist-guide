6aa0608dd3551dec154fea0dcd66fb7c05cea2de
Sarim Hassan <sarimhassan5@gmail.com>
2023-12-13 21:07:44 +0500

    added hotel card

--- src/components/cards/HotelCard.js
+++ src/components/cards/HotelCard.js
@@ -1,26 +1,45 @@
-import { Card, CardImg, CardBody, CardTitle, CardText, Button } from 'reactstrap';
+import { Button, Row, Col } from 'reactstrap';
 import { Rating } from 'react-simple-star-rating';
 
 const HotelCard = () => {
   return (
-    <Card className='mb-4 ms-4' style={{ width: '100%', maxWidth: '400px' }}>
-      <CardImg
-        top
-        width='20%'
-        src='https://picsum.photos/300/200'
-        alt='Hotel Image'
-        style={{ objectFit: 'cover' }}
-      />
-      <CardBody style={{ width: '100%' }}>
-        <div className='d-flex justify-content-between'>
-          <CardTitle tag='h3'>Hotel Name</CardTitle>
-          <Rating size={20} initialValue={4} readonly />
+    <Row
+      className='rounded shadow-lg m-md-20'
+      style={{ background: 'white', marginTop: '30px', marginBottom: '30px' }}
+    >
+      <Col md={4} className='d-flex align-items-center justify-content-center'>
+        <img
+          src='https://picsum.photos/300/200'
+          alt='Hotel Image'
+          style={{ objectFit: 'cover', height: '250px', width: '100%' }}
+        />
+      </Col>
+      <Col
+        md={8}
+        className='d-flex flex-column justify-content-center p-md-10'
+        style={{ height: '250px', overflow: 'scroll' }}
+      >
+        <div className='d-flex align-items-center justify-content-between'>
+          <h1>Pearl Continental</h1>
+          <div>
+            <Rating readonly size={20} initialValue={4} />
+          </div>
         </div>
-        <CardText>Location: City, Country</CardText>
-        <CardText>2 Nights - $200</CardText>
-        <Button color='primary'>Book Now</Button>
-      </CardBody>
-    </Card>
+        <div>Location: Bhurban</div>
+        <hr />
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </div>
+        <div className='d-flex justify-content-end'>
+          <Button color='primary'>Book Room</Button>
+        </div>
+      </Col>
+    </Row>
   );
 };
 export default HotelCard;
--- src/views/hotels/index.js
+++ src/views/hotels/index.js
@@ -1,8 +1,12 @@
 import { HotelCard } from '../../components';
+import HotelBanner from '../../assets/images/banners/Hotel-banner.jpeg';
+import { DynamicBanner } from '../../components';
 
 const Hotels = () => {
   return (
-    <div className='d-flex flex-wrap align-items-center'>
+    <div>
+      <DynamicBanner image={HotelBanner} text={'Hotel and Stays'} />
+
       {Array(5)
         .fill(0)
         .map(() => {
