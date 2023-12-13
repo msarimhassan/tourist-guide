86d2531b7adca649ac33ee597be9b312353dc366
Sarim Hassan <sarimhassan5@gmail.com>
2023-12-14 00:09:40 +0500

    updated the room screeen

--- src/views/single-hotel/index.js
+++ src/views/single-hotel/index.js
@@ -5,6 +5,40 @@ import { Calendar, Clock } from 'react-feather';
 import HotelBanner from '../../assets/images/banners/Hotel-banner.jpeg';
 import { DynamicBanner } from '../../components';
 import { Icons } from '../../common';
+import Arrow from '../../assets/images/Arrow.png';
+import Breakfast from '../../assets/images/breakfast.png';
+import Wifi from '../../assets/images/wifi.png';
+import Parking from '../../assets/images/parking.png';
+
+import Luxury from '../../assets/images/rooms/luxury.png';
+import Family from '../../assets/images/rooms/family.png';
+import Master from '../../assets/images/rooms/master.png';
+import Premium from '../../assets/images/rooms/premium.png';
+
+const hotelData = [Breakfast, Wifi, Parking];
+
+const roomTypes = [
+  {
+    image: Luxury,
+    type: 'Luxury Room',
+    price: '30,000',
+  },
+  {
+    image: Family,
+    type: 'Family Room',
+    price: '30,000',
+  },
+  {
+    image: Master,
+    type: 'Master Room',
+    price: '30,000',
+  },
+  {
+    image: Premium,
+    type: 'Luxury Room',
+    price: '30,000',
+  },
+];
 
 const SingleHotel = () => {
   const { IO5, MD, FA } = Icons;
@@ -12,10 +46,10 @@ const SingleHotel = () => {
     <div>
       <DynamicBanner image={HotelBanner} text={'The Blue Green Vacation Foundation'} />
 
-      <Row className='mt-3'>
+      {/* <Row className='mt-3'>
         <Col md={8}>
           <div>
-            <h2>The Blue Green Fountain</h2>
+            <h2 style={{ color: '#83B5D1' }}>The Blue Green Fountain</h2>
           </div>
 
           <div className='d-flex flex-wrap justify-content-start align-items-center'>
@@ -62,6 +96,77 @@ const SingleHotel = () => {
             </CardBody>
           </Card>
         </Col>
+      </Row> */}
+
+      <Row className='mt-3' style={{ paddingLeft: '20px', paddingRight: '20px' }}>
+        <Col md={6}>
+          <img src={Arrow} style={{ width: '100px' }} />
+          <h1 className='mt-2' style={{ color: '#83B5D1' }}>
+            The Blue Green Fountain
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum.
+          </p>
+        </Col>
+        <Col md={6} className='mt-2'>
+          <img
+            src='https://picsum.photos/300/200'
+            style={{ objectFit: 'cover', width: '100%', height: '300px' }}
+          />
+        </Col>
+      </Row>
+
+      {/* Room booking include */}
+
+      <div className='mt-5' style={{ background: '#E5EDF3', width: '100%', padding: '40px' }}>
+        <div className='ms-1'>
+          <img src={Arrow} style={{ width: '100px' }} />
+          <h2 className='mt-1' style={{ color: '#317EA7' }}>
+            Our Room Booking Include
+          </h2>
+        </div>
+
+        <div className='d-flex align-items-center justify-content-around mt-3 flex-wrap'>
+          {hotelData.map((image, index) => {
+            return <img key={index} style={{ width: '150px', height: '180px' }} src={image} />;
+          })}
+        </div>
+      </div>
+
+      <div className='ms-1 mt-5'>
+        <img src={Arrow} style={{ width: '100px' }} />
+        <h1 className='mt-2' style={{ color: '#317EA7' }}>
+          Select Room
+        </h1>
+      </div>
+
+      <Row>
+        {roomTypes.map((type) => {
+          return (
+            <Col className='mt-2' style={{ position: 'relative' }} md={6}>
+              <img
+                style={{ width: '100%', height: '400px', objectFit: 'cover', borderRadius: '10px' }}
+                src={type.image}
+              />
+              <div style={{ position: 'absolute', top: 0, background: '#F5F5F5', padding: '5px' }}>
+                <h1 style={{ color: '#575757' }}>PKR {type.price}</h1>
+              </div>
+              <div style={{ position: 'absolute', bottom: 0, padding: '5px' }}>
+                <h1 style={{ color: 'white' }}>{type.type}</h1>
+              </div>
+            </Col>
+          );
+        })}
       </Row>
     </div>
   );
